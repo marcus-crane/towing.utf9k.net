8d8ecd062f1fdd010b40b1ef454113546d0b4f7d
Marcus Crane <marcus@utf9k.net>
2023-06-10 16:51:41 +1200

    General tidying up
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- app.jsx
+++ app.jsx
@@ -1,35 +1,35 @@
 /* global window */
-import React, {useState, useEffect} from 'react';
-import {render} from 'react-dom';
-import {StaticMap} from 'react-map-gl';
-import {AmbientLight, PointLight, LightingEffect} from '@deck.gl/core';
-import DeckGL from '@deck.gl/react';
-import {PolygonLayer} from '@deck.gl/layers';
-import {TripsLayer} from '@deck.gl/geo-layers';
+import React, { useState, useEffect } from "react";
+import { StaticMap } from "react-map-gl";
+import { AmbientLight, PointLight, LightingEffect } from "@deck.gl/core";
+import DeckGL from "@deck.gl/react";
+import { PolygonLayer } from "@deck.gl/layers";
+import { TripsLayer } from "@deck.gl/geo-layers";
 
 // Source data CSV
 const DATA_URL = {
-  TRIPS: 'https://raw.githubusercontent.com/marcus-crane/towing.utf9k.net/main/routes.json' // eslint-disable-line
+  TRIPS:
+    "https://raw.githubusercontent.com/marcus-crane/towing.utf9k.net/main/public/routes.json", // eslint-disable-line
 };
 
 const ambientLight = new AmbientLight({
   color: [255, 255, 255],
-  intensity: 1.0
+  intensity: 1.0,
 });
 
 const pointLight = new PointLight({
   color: [255, 255, 255],
   intensity: 2.0,
-  position: [174.76, -36.84, 8000]
+  position: [174.76, -36.84, 8000],
 });
 
-const lightingEffect = new LightingEffect({ambientLight, pointLight});
+const lightingEffect = new LightingEffect({ ambientLight, pointLight });
 
 const material = {
   ambient: 0.1,
   diffuse: 0.6,
   shininess: 32,
-  specularColor: [60, 64, 70]
+  specularColor: [60, 64, 70],
 };
 
 const DEFAULT_THEME = {
@@ -37,7 +37,7 @@ const DEFAULT_THEME = {
   trailColor0: [253, 128, 93],
   trailColor1: [23, 184, 190],
   material,
-  effects: [lightingEffect]
+  effects: [lightingEffect],
 };
 
 const INITIAL_VIEW_STATE = {
@@ -45,18 +45,19 @@ const INITIAL_VIEW_STATE = {
   latitude: -36.86103,
   zoom: 14,
   pitch: 45,
-  bearing: 0
+  bearing: 0,
 };
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 
 const landCover = [
   [
     [174.7, -36.8],
     [174.7, -36.82],
     [174.76, -36.82],
-    [174.76,-36.8]
-  ]
+    [174.76, -36.8],
+  ],
 ];
 
 export default function App({
@@ -66,13 +67,13 @@ export default function App({
   mapStyle = MAP_STYLE,
   theme = DEFAULT_THEME,
   loopLength = 30000, // unit corresponds to the timestamp in source data
-  animationSpeed = 15
+  animationSpeed = 15,
 }) {
   const [time, setTime] = useState(0);
   const [animation] = useState({});
 
   const animate = () => {
-    setTime(t => (t + animationSpeed) % loopLength);
+    setTime((t) => (t + animationSpeed) % loopLength);
     animation.id = window.requestAnimationFrame(animate);
   };
 
@@ -84,25 +85,25 @@ export default function App({
   const layers = [
     // This is only needed when using shadow effects
     new PolygonLayer({
-      id: 'ground',
+      id: "ground",
       data: landCover,
-      getPolygon: f => f,
+      getPolygon: (f) => f,
       stroked: false,
-      getFillColor: [0, 0, 0, 0]
+      getFillColor: [0, 0, 0, 0],
     }),
     new TripsLayer({
-      id: 'trips',
+      id: "trips",
       data: trips,
-      getPath: d => d.path,
-      getTimestamps: d => d.timestamps,
-      getColor: d => (d.vendor === 0 ? theme.trailColor0 : theme.trailColor1),
+      getPath: (d) => d.path,
+      getTimestamps: (d) => d.timestamps,
+      getColor: (d) => (d.vendor === 0 ? theme.trailColor0 : theme.trailColor1),
       opacity: 0.3,
       widthMinPixels: 2,
       rounded: true,
       trailLength,
       currentTime: time,
-      shadowEnabled: false
-    })
+      shadowEnabled: false,
+    }),
   ];
 
   return (
@@ -116,5 +117,3 @@ export default function App({
     </DeckGL>
   );
 }
-
-render(<App />, document.getElementById("app"));
